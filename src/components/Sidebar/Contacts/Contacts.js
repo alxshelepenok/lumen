ce0306a14f62a30a09303cfbd8e42da9244e75ff
Siwalik Mukherjee <20061595+siwalikm@users.noreply.github.com>
2022-03-27 19:02:07 +0200

    Fix console warnings
    Fixing console warning `Warning: Each child in a list should have a unique "key" prop.`
    ```
    Warning: Each child in a list should have a unique "key" prop
    Check the render method of `Contacts`. See https://reactjs.org/link/warning-keys for more information.
        at a
        at Contacts (webpack-internal:///./src/components/Sidebar/Contacts/Contacts.js:20:23)
    ```

--- src/components/Sidebar/Contacts/Contacts.js
+++ src/components/Sidebar/Contacts/Contacts.js
@@ -13,13 +13,14 @@ type Props = {
 const Contacts = ({ contacts }: Props) => (
   <div className={styles['contacts']}>
     <ul className={styles['contacts__list']}>
-      {Object.keys(contacts).map((name) => (!contacts[name] ? null : (
+      {Object.keys(contacts).map((name, key) => (!contacts[name] ? null : (
         <li className={styles['contacts__list-item']} key={name}>
           <a
             className={styles['contacts__list-item-link']}
             href={getContactHref(name, contacts[name])}
             rel="noopener noreferrer"
             target="_blank"
+            key={key}
           >
             <Icon name={name} icon={getIcon(name)} />
           </a>
