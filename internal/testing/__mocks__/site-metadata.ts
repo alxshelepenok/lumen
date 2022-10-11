02cbd6a3d971aca54da5a7acd7f1b1453811cb84
Alyssa Feola <65581330+its-a-lisa@users.noreply.github.com>
2022-10-11 15:16:55 -0400

    fixed url

--- internal/testing/__mocks__/site-metadata.ts
+++ internal/testing/__mocks__/site-metadata.ts
@@ -4,7 +4,7 @@ import menu from "./menu";
 export default {
   site: {
     siteMetadata: {
-      url: "https://its-a-lisa.com"",
+      url: "https://its-a-lisa.com",
       title: "Blog by Alyssa Feola",
       subtitle:
         "A place for my semi-strong opinions, general musings and many, many questions.",
