19c8009b742cfb0f8ae4d2bb6038f5a1ff2d0cac
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2023-08-18 09:43:36 -0500

    Revert "Create index.js"
    This reverts commit a4ee1be59bfba06f00090f9595e09ca4ec7cbf0f.

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-    window.onload = function() {
-        var sidebarWidth = document.querySelector('.sidenav').offsetWidth;
-        document.querySelector('.page').style.marginLeft = sidebarWidth + 'px';
-    }
-    
-    window.onresize = function() {
-        var sidebarWidth = document.querySelector('.sidenav').offsetWidth;
-        document.querySelector('.page').style.marginLeft = sidebarWidth + 'px';
-    }
