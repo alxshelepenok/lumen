98435cec953e5e753df24254b92683c3891e8ee3
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2023-10-31 00:18:51 -0500

    Update Content.tsx

--- src/components/Post/Content/Content.tsx
+++ src/components/Post/Content/Content.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import * as styles from "./Content.module.scss";
 
@@ -8,8 +8,25 @@ interface Props {
 }
 
 const Content: React.FC<Props> = ({ body, title }: Props) => (
+    const [data, setData] = useState(''); // State to store the XHR data
+
+  useEffect(() => {
+    // Make an XHR request on page load
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.countapi.xyz/get/unaligned.world/:PATHNAME:', true);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        // Parse the response data (assuming it's JSON)
+        const responseData = JSON.parse(xhr.responseText);
+        setData(responseData.value); // Update the state with the data
+      }
+    };
+    xhr.send();
+  }, []);
+
   <div className={styles.content}>
     <h1 className={styles.title}>{title}</h1>
+    <p>{data}</p>
     <div className={styles.body} dangerouslySetInnerHTML={{ __html: body }} />
   </div>
 );
