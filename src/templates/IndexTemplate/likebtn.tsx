69b368802edd2a2c69f17c45a977f6a98d5782a0
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2023-10-30 22:19:38 -0500

    Create likebtn.tsx

---
+++ src/templates/IndexTemplate/likebtn.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+
+const LikeBtnScript = () => {
+  useEffect(() => {
+    if (document.getElementById("likebtn_wjs")) return;
+    const a = document.createElement("script");
+    const m = document.getElementsByTagName("script")[0];
+    a.async = 1;
+    a.id = "likebtn_wjs";
+    a.src = "//w.likebtn.com/js/w/widget.js";
+    m.parentNode.insertBefore(a, m);
+  }, []);
+
+  return null;
+};
+
+export default LikeBtnScript;
