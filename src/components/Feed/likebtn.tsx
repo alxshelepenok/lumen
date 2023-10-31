73fd2dcf8e74f243452851296da5f71b443c24c4
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2023-10-30 23:18:27 -0500

    Update likebtn.tsx

--- src/components/Feed/likebtn.tsx
+++ src/components/Feed/likebtn.tsx
@@ -1,21 +1,7 @@
 import React, { useEffect } from "react";
 
 const likebtn = () => {
-  useEffect(() => {
-    document.addEventListener("DOMContentLoaded", function () {
-      if (document.getElementById("likebtn_wjs")) return;
-      const a = document.createElement("script");
-      const m = document.getElementsByTagName("script")[0];
-      a.async = 1;
-      a.id = "likebtn_wjs";
-      a.src = "//w.likebtn.com/js/w/widget.js";
-      console.log("HOLY FUCKING SHIT!!!!")
-      print()
-      m.parentNode.insertBefore(a, m);
-    });
-  }, []);
-
-  return null;
+console.log("TEST")
 };
 
 export default likebtn;
