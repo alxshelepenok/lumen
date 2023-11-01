20d9c334fc8184c5debd78381189f46e4e4ba5fe
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2023-10-31 22:19:01 -0500

    Update LikeButton.jsx

--- src/components/Post/LikeButton.jsx
+++ src/components/Post/LikeButton.jsx
@@ -31,7 +31,7 @@ export class LikeButton extends Component {
 
   updateCount(change) {
     // Make an XHR request to the CountAPI to increment or decrement the count
-    const url = `https://script.google.com/macros/s/AKfycbyVFEgIz5t272FQjoNHD825XTcNlxDnUu1jABzz2A5KCuzhWdwbRYrTbwPjzMDsycDPKQ/exec?POST_NAME=${window.location.pathname.split("/")[window.location.pathname.split("/").length-1]}&ACTION=${change}`;
+    const url = `https://script.google.com/macros/s/AKfycbx0JirEyCRDrJ_lLIjg_dsVbvDj5PjZV9hJBGhP13Eft--Ymal8YzqvFX82sVjJEJ475g/exec?POST_NAME=${window.location.pathname.split("/")[window.location.pathname.split("/").length-1]}&ACTION=${change}`;
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.send();
