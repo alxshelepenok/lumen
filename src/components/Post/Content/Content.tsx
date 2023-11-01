522ea19cc105460f444a8e0d74ea72873e9e9f6a
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2023-11-01 01:14:29 -0500

    Update Content.tsx

--- src/components/Post/Content/Content.tsx
+++ src/components/Post/Content/Content.tsx
@@ -7,9 +7,15 @@ interface Props {
   body: string;
 }
 
+function readingTime(text) {
+  const words = text.trim().split(/\s+/).length;
+  return Math.ceil(words / 225);
+}
+
 const Content: React.FC<Props> = ({ body, title }: Props) => (
   <div className={styles.content}>
     <h1 className={styles.title}>{title}</h1>
+    <i>{readingTime(body) + " minutes"}</i>
     <div className={styles.body} dangerouslySetInnerHTML={{ __html: body }} />
   </div>
 );
